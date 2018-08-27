84c4561ea5b575a740bda9e90fcf03de90d48506
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-08-26 22:42:24 -0700

    Add show page and list gists of a gist (one gist user)

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,2 +1,15 @@
 module GistsHelper
+  def gist_title(gist)
+    link = link_to icon('external-link'), gist.url, rel: "nofollow"
+    github = 'gist.github.com/'
+    safe_join [github, gist.name, ' ', link]
+  end
+
+  def gist_name(gh_gist)
+    if gh_gist['description'].present?
+      gh_gist['description']
+    else
+      gh_gist['files'].first.first
+    end
+  end
 end
