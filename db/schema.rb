69d91ff55b8c4ad41c60a9f34f91075c8dc4b01e
kangkyu <kangkyu1111@gmail.com>
2022-04-23 21:42:08 -0700

    Run app:update and rails tmp:cache:clear

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_10_20_033131) do
-
+ActiveRecord::Schema[7.0].define(version: 2015_10_20_033131) do
   create_table "gists", force: :cascade do |t|
     t.string "name"
     t.string "url"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
