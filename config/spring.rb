685c49ea846103a8d6c8e7d63d2dd7bf4eff05be
kangkyu <kangkyu1111@gmail.com>
2022-04-23 20:59:51 -0700

    rails app:update - Now Rails 6.0.4.7

--- config/spring.rb
+++ config/spring.rb
@@ -1,7 +1,7 @@
-%w[
-  .ruby-version
-  .tool-versions
-  .rbenv-vars
-  tmp/restart.txt
-  tmp/caching-dev.txt
-].each { |path| Spring.watch(path) }
+Spring.watch(
+  ".ruby-version",
+  ".tool-versions"
+  ".rbenv-vars",
+  "tmp/restart.txt",
+  "tmp/caching-dev.txt"
+)
